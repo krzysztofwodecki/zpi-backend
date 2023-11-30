223d298f7feca66418ccad8d426b98dff1e4cffe
Konrad <konrad.ryczek@student.up.krakow.pl>
2023-11-30 01:39:22 +0100

    Added Spring and PgAdmin as docker services

--- README.md
+++ README.md
@@ -2,3 +2,32 @@
 - Spring Boot,
 - skrypty migracyjne,
 - kod SQL to stworzenia bazki.
+
+# Oprogramowanie
+
+- Java 17 (openjdk version "17.0.4.1")
+- Maven (Apache Maven 3.9.5)
+- Docker (Docker version 20.10.17)
+
+# Uruchamianie backendu
+
+Spakowanie backendu do pliku .jar <br>
+```bash
+./mvnw clean package -DskipTests
+``` 
+
+Uruchomienie kontenerów <br>
+```bash 
+docker-compose up
+``` 
+
+Usunięcie niepotrzebnych kontenerów <br>
+```bash 
+docker-compose down
+```
+
+# Działające usługi
+
+- PostgreSQL
+- Spring
+- PgAdmin
---
+++ src/main/java/com/example/zpi/MainBackendApplication.java
@@ -0,0 +1,32 @@
+package com.example.zpi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MainBackendApplication {
+
+  private static final Logger log = LoggerFactory.getLogger(MainBackendApplication.class);
+
+  public static void main(String[] args) {
+    SpringApplication.run(MainBackendApplication.class);
+  }
+
+  // Bean that fills db with sample data
+  @Bean
+  public CommandLineRunner demo(CustomerRepository repository) {
+    return (args) -> {
+      // save sample customers
+      repository.save(new Customer("Jack", "Bauer"));
+      repository.save(new Customer("Chloe", "O'Brian"));
+      repository.save(new Customer("Kim", "Bauer"));
+      repository.save(new Customer("David", "Palmer"));
+      repository.save(new Customer("Michelle", "Dessler"));
+    };
+  }
+
+}
