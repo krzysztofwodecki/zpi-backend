bfed4969a828f132a24d1b560669c37e7a41b8a4
Konrad <konrad.ryczek@student.up.krakow.pl>
2023-12-14 00:02:24 +0100

    Added multi-stage mvn build and fixed jpa errors

--- src/main/java/com/example/zpi/entities/EventEntity.java
+++ src/main/java/com/example/zpi/entities/EventEntity.java
@@ -1,13 +1,15 @@
 package com.example.zpi.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 
 @Entity
 public class EventEntity {
+
+    protected EventEntity() {}
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
