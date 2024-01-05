6187db0b7d02082db9283da6f2332c9e8f4d1c44
Sotakebk <81926249+Sotakebk@users.noreply.github.com>
2024-01-05 15:54:24 +0100

    /rewards/redeemed

--- src/main/java/com/example/zpi/controllers/RewardsController.java
+++ src/main/java/com/example/zpi/controllers/RewardsController.java
@@ -1,31 +1,43 @@
 package com.example.zpi.controllers;
 
-import com.example.zpi.entities.AttendanceEntity;
+import com.example.zpi.entities.RedeemedRewardEntity;
 import com.example.zpi.entities.RewardEntity;
 import com.example.zpi.service.EventService;
 import com.example.zpi.service.RewardService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 @RestController
 @RequestMapping("/rewards")
 public class RewardsController {
     final private RewardService rewardService;
+    final private EventService eventService;
 
-    public RewardsController(RewardService rewardService) {
+    public RewardsController(RewardService rewardService, EventService eventService) {
         this.rewardService = rewardService;
+        this.eventService = eventService;
     }
 
     @GetMapping("/")
     public ResponseEntity<List<RewardEntity>> getAvailableRewards() {
         List<RewardEntity> rewards = rewardService.getAllRewards();
+        return ResponseEntity.ok(rewards);
+    }
+
+    @GetMapping("/redeemed")
+    public ResponseEntity<List<RedeemedRewardEntity>> getRedeemedRewards(@RequestParam(required = true) Long userId) {
+        try{
+            eventService.getUserById(userId);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+        List<RedeemedRewardEntity> rewards = rewardService.getRedeemedRewards(userId);
         return ResponseEntity.ok(rewards);
     }
 }
--- src/main/java/com/example/zpi/repositories/RedeemedRewardRepository.java
+++ src/main/java/com/example/zpi/repositories/RedeemedRewardRepository.java
@@ -1,8 +1,10 @@
 package com.example.zpi.repositories;
 
-import com.example.zpi.entities.RedeemedReward;
-import com.example.zpi.entities.RewardEntity;
+import com.example.zpi.entities.RedeemedRewardEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface RedeemedRewardRepository extends CrudRepository<RedeemedRewardRepository, Long> {
+    public List<RedeemedRewardEntity>  getRedeemedRewardsByUserId(Long userId);
 }
